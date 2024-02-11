1cd38c91801b2d067ef6ba5ac3cbe82ba231a05d
LadyVamp <ladyvamp666@gmail.com>
2024-02-11 18:39:02 +0300

    fix warning

--- src/components/Recipes/RecipeDetails.vue
+++ src/components/Recipes/RecipeDetails.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="row">
-    <div class="col-6 col-md-6 col-xs-12 justify-start">
-      <h2>{{ currentRecipe.title }}</h2>
-      <div>
-        <q-img :src="'/recipe-images/' + currentRecipe.id + '.jpg'" width="400px" />
+  <div v-if="!isLoading">
+    <div class="row">
+      <div class="col-6 col-md-6 col-xs-12 justify-start">
+        <h2>{{ currentRecipe.title }}</h2>
+        <div>
+          <q-img :src="'/recipe-images/' + currentRecipe.id + '.jpg'" width="400px" />
+        </div>
+      </div>
+      <div class="col-6 col-md-6 col-xs-12 justify-start">
+        TODO: ссылки<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quod neque fuga nostrum, enim vero itaque
+        sed cupiditate necessitatibus et eius vitae accusamus tenetur, dolor ratione amet corporis? Distinctio,
+        voluptatum.
       </div>
     </div>
-    <div class="col-6 col-md-6 col-xs-12 justify-start">
-      TODO: ссылки<br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quod neque fuga nostrum, enim vero itaque
-      sed cupiditate necessitatibus et eius vitae accusamus tenetur, dolor ratione amet corporis? Distinctio,
-      voluptatum.
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12 col-md-12 col-xs-12 justify-start">
-      <IconNature :nature="currentRecipe.nature" />
-      <IconFeature :feature="currentRecipe.feature" />
-      <IconSeason :season="currentRecipe.season" />
-      <div v-if="currentRecipe.time">
-        <q-icon name="mdi-timer-outline" color="primary" size="32px" title="Время приготовления" />
-        {{ currentRecipe.time }} минут
+    <div class="row">
+      <div class="col-12 col-md-12 col-xs-12 justify-start">
+        <IconNature :nature="currentRecipe.nature" />
+        <IconFeature :feature="currentRecipe.feature" />
+        <IconSeason :season="currentRecipe.season" />
+        <div v-if="currentRecipe.time">
+          <q-icon name="mdi-timer-outline" color="primary" size="32px" title="Время приготовления" />
+          {{ currentRecipe.time }} минут
+        </div>
+        <p>
+          <q-icon name="mdi-account-multiple" color="primary" size="32px" title="Количество порций" />
+          {{ currentRecipe.servings }}
+        </p>
+        <p class="text-green-10" v-if="currentRecipe.note">{{ currentRecipe.note }}</p>
+        <h3>Ингредиенты</h3>
+        <ul v-for="(value, name, idx) in currentRecipe.ingredients" :key="idx">
+          <li>{{ name }} – {{ value }}</li>
+        </ul>
+        <h3>Приготовление</h3>
+        <ul v-for="(value, name, idx) in currentRecipe.steps" :key="idx">
+          <li>{{ name }}. {{ value }}</li>
+        </ul>
       </div>
-      <p>
-        <q-icon name="mdi-account-multiple" color="primary" size="32px" title="Количество порций" />
-        {{ currentRecipe.servings }}
-      </p>
-      <p class="text-green-10" v-if="currentRecipe.note">{{ currentRecipe.note }}</p>
-      <h3>Ингредиенты</h3>
-      <ul v-for="(value, name, idx) in currentRecipe.ingredients" :key="idx">
-        <li>{{ name }} – {{ value }}</li>
-      </ul>
-      <h3>Приготовление</h3>
-      <ul v-for="(value, name, idx) in currentRecipe.steps" :key="idx">
-        <li>{{ name }}. {{ value }}</li>
-      </ul>
     </div>
   </div>
 </template>
@@ -57,16 +59,21 @@ export default defineComponent({
   data() {
     return {
       currentRecipe: {} as Recipe,
+      isLoading: false,
     };
   },
-  mounted() {
+  created() {
     this.loadRecipes();
   },
   methods: {
     loadRecipes() {
-      getAllRecipes().then((res: RecipesResponse) => {
-        this.currentRecipe = res.recipes.filter((item) => item.id === this.$route.params.id)[0];
-      });
+      this.isLoading = true;
+      getAllRecipes()
+        .then((res: RecipesResponse) => {
+          this.currentRecipe = res.recipes.filter((item) => item.id === this.$route.params.id)[0];
+        })
+        .catch((err) => console.error(err))
+        .finally(() => (this.isLoading = false));
     },
   },
 });
