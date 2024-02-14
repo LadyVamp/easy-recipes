6445ba18b0ac704e1d9bd178bed3f2fb50befee4
LadyVamp <ladyvamp666@gmail.com>
2024-02-14 18:49:00 +0300

    linkToProductAnotherShop

--- src/components/Recipes/RecipeDetails.vue
+++ src/components/Recipes/RecipeDetails.vue
@@ -10,9 +10,13 @@
       <div class="col-6 col-md-6 col-xs-12 justify-start">
         <div class="row">
           <div class="col-6">
-            <q-toggle v-model="isShowLinksSber" label="Ссылки на Сбермаркет" />
-            <q-select v-model="selectedShop" :options="shops" label="Магазин" />
+            <q-toggle v-model="isShowLinksSM" label="Ссылки на Сбермаркет" :disable="isShowLinksAnotherShop" />
+            <q-select v-model="selectedShopSM" :options="shopsSM" label="Магазин" />
             <q-select v-model="selectedSort" :options="sortOptions" label="Сортировка" />
+          </div>
+          <div class="col-6">
+            <q-toggle v-model="isShowLinksAnotherShop" label="Ссылки на магазин" :disable="isShowLinksSM" />
+            <q-select v-model="selectedShop" :options="shops" label="Магазин" />
           </div>
         </div>
       </div>
@@ -32,15 +36,24 @@
         </p>
         <p class="text-green-10" v-if="currentRecipe.note">{{ currentRecipe.note }}</p>
         <h3>Ингредиенты</h3>
-        <div v-if="!isShowLinksSber">
+        <div v-if="!isShowLinksSM && !isShowLinksAnotherShop">
           <ul v-for="(value, name, idx) in currentRecipe.ingredients" :key="idx">
             <li>{{ name }} – {{ value }}</li>
           </ul>
         </div>
         <!-- TODO: ссылки для || -->
-        <div v-if="isShowLinksSber">
+        <div v-if="isShowLinksSM">
           <ul v-for="(value, name, idx) in currentRecipe.ingredients" :key="idx">
-            <a :href="linkToProductInSber(name)" target="_blank">{{ name }}</a>
+            <a :href="linkToProductInSM(name)" target="_blank">{{ name }}</a>
+            –
+            {{
+              value
+            }}
+          </ul>
+        </div>
+        <div v-if="isShowLinksAnotherShop">
+          <ul v-for="(value, name, idx) in currentRecipe.ingredients" :key="idx">
+            <a :href="linkToProductAnotherShop(name)" target="_blank">{{ name }}</a>
             –
             {{
               value
@@ -75,14 +88,14 @@ export default defineComponent({
     return {
       currentRecipe: {} as Recipe,
       isLoading: false,
-      isShowLinksSber: false,
-      shops: [
+      isShowLinksSM: false,
+      shopsSM: [
         { value: 'auchan', label: 'Ашан' },
         { value: 'lenta', label: 'Лента' },
         { value: 'globus', label: 'Глобус' },
         { value: 'okey', label: 'Окей' },
       ],
-      selectedShop: { value: 'auchan', label: 'Ашан' },
+      selectedShopSM: { value: 'auchan', label: 'Ашан' },
       sortOptions: [
         { value: 'popularity', label: 'По популярности' },
         { value: 'price_asc', label: 'Сначала дешевые' },
@@ -90,6 +103,23 @@ export default defineComponent({
         { value: 'unit_price_asc', label: 'Выгоднее по весу' },
       ],
       selectedSort: { value: 'unit_price_asc', label: 'Выгоднее по весу' },
+      isShowLinksAnotherShop: false,
+      selectedShop: { value: 'vprok', label: 'Впрок' },
+      shops: [
+        { value: 'vprok', label: 'Впрок', link: 'https://www.vprok.ru/catalog/search?text={ingredient}' },
+        { value: 'metro', label: 'Метро', link: 'https://online.metro-cc.ru/search?q={ingredient}' },
+        { value: 'lenta', label: 'Лента', link: 'https://moscow.online.lenta.com/search/{ingredient}' },
+        {
+          value: 'perekrestok',
+          label: 'Перекресток',
+          link: 'https://www.perekrestok.ru/cat/search?search={ingredient}',
+        },
+        {
+          value: 'vprokMarket',
+          label: 'Впрок Я.Маркет',
+          link: 'https://shop.market.yandex.ru/retail/vprok_ru?placeSlug=vprokru_omknq&query={ingredient}&standaloneType=shop',
+        },
+      ],
     };
   },
   created() {
@@ -105,14 +135,19 @@ export default defineComponent({
         .catch((err) => console.error(err))
         .finally(() => (this.isLoading = false));
     },
-    linkToProductInSber(ingredient: string) {
+    linkToProductInSM(ingredient: string) {
       const url =
         'https://sbermarket.ru/' +
-        `${this.selectedShop.value}` +
+        `${this.selectedShopSM.value}` +
         '/search?keywords=' +
         `${ingredient.trim()}` +
         `&sort=` +
         `${this.selectedSort.value}`;
+      return url;
+    },
+    linkToProductAnotherShop(ingredient: string) {
+      let url = this.shops.find((item) => item.value === this.selectedShop.value)!.link;
+      url = url.replace('{ingredient}', ingredient);
       return url;
     },
   },
